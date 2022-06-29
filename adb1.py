c72c619caa3f6eb872cdff77ef12d3c56167da21
gokulganesh11 <tkgokulganesh@gmail.com>
2022-06-29 07:54:44 +0530

    first_commit

---
+++ adb1.py
@@ -0,0 +1,10 @@
+import os
+import time
+os.system("adb shell input tap 382 1333")
+os.system("adb shell input tap 243 1229")
+time.sleep(5)
+os.system("adb shell input tap 208 415")
+time.sleep(5)
+os.system("adb shell input tap 366 1700")
+time.sleep(5)
+os.system("adb shell input tap 760 1678")
